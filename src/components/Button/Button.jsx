33d7d4299d0fb90f8b3887b1f99028b609a5dc79
Kesha Bocharov <keshabocharov1221@gmail.com>
2025-08-22 03:17:18 +0300

    started header, and finished pre starting things

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,58 @@
+import './Button.scss'
+import classNames from 'classnames'
+import Icon from "@/components/Icon";
+
+const Button = (props) => {
+  const {
+    className,
+    type = 'button',
+    href,
+    target,
+    /*
+        '' (default) | 'transparent'
+        */
+    mode = '',
+    label,
+    isLabelHidden = false,
+    iconName,
+
+    /*
+    * before | after
+    * */
+
+    iconPosition = 'before',
+  } = props
+
+  const isLink = href !== undefined
+  const Componen = isLink ? 'a' : 'button'
+  const linkProps = {href, target}
+  const buttonProps = {type}
+  const specificProps = isLink ? linkProps : buttonProps
+  const title = isLabelHidden ? label : undefined
+  const iconComponent = iconName && (
+    <Icon
+      className='button__icon'
+      name={iconName}
+    />
+  )
+
+  return (
+    <Componen
+      className={classNames(className, 'button', {
+        [`button--${mode}`]: mode,
+      })}
+      title={title}
+      aria-label={title}
+      {...specificProps}
+    >
+      {iconPosition === 'before' && iconComponent}
+      {!isLabelHidden && (
+        <span className="button__label">{label}</span>
+      )}
+      {iconPosition === 'after' && iconComponent}
+
+    </Componen>
+  )
+}
+
+export default Button
