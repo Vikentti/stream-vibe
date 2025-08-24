1b548b6b3ecd94dda1f7b0046dbcb397265943b7
Kesha Bocharov <keshabocharov1221@gmail.com>
2025-08-24 21:11:29 +0300

    new section == question/ accordion finished

---
+++ src/components/AccordionGroup/AccordionGroup.jsx
@@ -0,0 +1,37 @@
+import './AccordionGroup.scss'
+import classNames from 'classnames'
+
+const AccordionGroup = (props) => {
+  const {
+    className,
+    columns = 1,
+    children,
+    isOrderedList = true,
+
+  } = props
+
+  const itemsPerColumn = Math.floor(children.length / columns)
+  const ListTag = isOrderedList ? 'ol' : 'ul'
+
+  return (
+    <ListTag
+      className={classNames('accordion-group', {
+        [`accordion-group--${columns}-columns`]: columns > 1,
+        'accordion-group--has-counter': isOrderedList,
+      })}
+    >
+      {children.map((child, index) => (
+        <li
+          className={classNames('accordion-group__item', {
+            'accordion-group__item--last-column-item': columns > 1 && (index + 1) % itemsPerColumn === 0
+          })}
+          key={index}
+        >
+          {child}
+        </li>
+      ))}
+    </ListTag>
+  )
+}
+
+export default AccordionGroup
